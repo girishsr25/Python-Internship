079e199661a2f6c7f93979eb1fcf7fdc464f0857
Girish Sairam <sgirish585@gmail.com>
2020-11-24 14:07:48 +0530

    Day 2

---
+++ Day 2 Task.py
@@ -0,0 +1,30 @@
+#Day 2 Task
+
+#Exercise 1
+
+a = b = c = 50
+
+a = a // 10
+b = b * 50
+c = c + 60
+
+print(a,b,c) #output = 5 2500 110
+
+
+#Exercise 2
+
+word = "ABCDE"
+
+word = word[:2] + 'G' + word[3:]
+
+print(word)  #output = ABGDE
+
+
+#Exercise 3
+
+a = 10
+
+b = 5.8
+
+print(float(a))  #output = 10.0
+print(int(b))    #output = 5
